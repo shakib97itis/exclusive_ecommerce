df1734adda77ad3df782a552d02f2eb5a781c13f
Shakibul Islam <shakib97itis@gmail.com>
2025-07-15 19:34:31 +0600

    add TypographyDisplay component and enhance index.css with new typography styles

---
+++ src/components/TypographyDisplay.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TypographyDisplay = () => {
+  return (
+    <div>
+        <h1 className="heading-110px font-bold">Heading 110px Bold</h1>
+        <h1 className="heading-110px font-semibold">Heading 110px Semibold</h1>
+        <h1 className="heading-110px font-medium">Heading 110px Medium</h1>
+        <h1 className="heading-110px font-normal">Heading 110px Normal</h1>
+    </div>
+  )
+}
+
+export default TypographyDisplay
