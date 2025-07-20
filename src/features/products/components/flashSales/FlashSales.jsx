2b4401b359dbc0bd1200c0296c8add29ba58932b
Shakibul Islam <shakib97itis@gmail.com>
2025-07-20 17:54:37 +0600

    feat: implement FlashSales component with SalesTimer, SalesCardSlider, and SalesCard

--- src/features/products/components/flashSales/FlashSales.jsx
+++ src/features/products/components/flashSales/FlashSales.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { FaArrowCircleLeft, FaArrowCircleRight } from "react-icons/fa";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa6";
+import SalesTimer from "./SalesTimer";
+import SalesCardSlider from "./SalesCardSlider";
 
 const FlashSales = () => {
   return (
@@ -7,54 +9,29 @@ const FlashSales = () => {
       {/* TOP TITLE */}
       <div className="title-16-semibold text-secondary-2 mb-6 flex items-center gap-2.5">
         <div className="bg-secondary-2 h-10 w-5 rounded-sm"></div>
-        <p c>Today's</p>
+        <p>Today's</p>
       </div>
-      <div className="flex items-center justify-between">
-        <div className="space-y-2.5">
+
+      <div className="flex items-center justify-between mb-10">
+        <div className="flex flex-col gap-2.5 md:flex-row md:items-center md:gap-20">
           <h2 className="heading-36-semibold">Flash Sales</h2>
           <SalesTimer />
         </div>
-        <div className="flex gap-2 items-center">
-          <button className="w-[46px] h-[46px] flex items-center justify-center cursor-pointer">
-            <FaArrowCircleLeft className="text-[46px]" />
+
+        <div className="flex items-center gap-2">
+          <button className="bg-secondary flex h-[46px] w-[46px] cursor-pointer items-center justify-center rounded-full">
+            <FaArrowLeft className="text-text-2 text-2xl" />
           </button>
-          <button className="w-[46px] h-[46px] flex items-center justify-center cursor-pointer">
-            <FaArrowCircleRight className="text-[46px]" />
+
+          <button className="bg-secondary flex h-[46px] w-[46px] cursor-pointer items-center justify-center rounded-full">
+            <FaArrowRight className="text-text-2 text-2xl" />
           </button>
         </div>
       </div>
+
+      <SalesCardSlider />
     </div>
   );
-};
-
-const SalesTimer = () => {
-  return (
-    <div className="flex items-end gap-5">
-      <div className="">
-        <div className="title-12-medium text-text-2 mb-1">Days</div>
-        <div className="heading-32-bold text-text-2">03</div>
-      </div>
-      <span className="text-hover-button heading-32-bold mb-1">:</span>
-      <div className="">
-        <div className="title-12-medium text-text-2 mb-1">Hours</div>
-        <div className="heading-32-bold text-text-2">23</div>
-      </div>
-      <span className="text-hover-button heading-32-bold mb-1">:</span>
-      <div className="">
-        <div className="title-12-medium text-text-2 mb-1">Minutes</div>
-        <div className="heading-32-bold text-text-2">19</div>
-      </div>
-      <span className="text-hover-button heading-32-bold mb-1">:</span>
-      <div className="">
-        <div className="title-12-medium text-text-2 mb-1">Seconds</div>
-        <div className="heading-32-bold text-text-2">56</div>
-      </div>
-    </div>
-  );
-};
-
-const SalesSlider = () => {
-    
 };
 
 export default FlashSales;
