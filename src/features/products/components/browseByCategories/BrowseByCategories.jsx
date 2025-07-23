111aaea001bbaa5972e893db913525d85fd79bc2
Shakibul Islam <shakib97itis@gmail.com>
2025-07-23 23:30:36 +0600

    feat: add BrowseByCategories component with CategorySlider and CategoryCard for improved category browsing experience

---
+++ src/features/products/components/browseByCategories/BrowseByCategories.jsx
@@ -0,0 +1,83 @@
+import React, { useRef } from "react";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa6";
+import CategorySlider from "./CategorySlider";
+
+const BrowsByCategories = () => {
+  const sliderRef = useRef(null);
+  const goToNextSlide = () => sliderRef.current.slickNext();
+  const goToPrevSlide = () => sliderRef.current.slickPrev();
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          initialSlide: 0,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 0,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          initialSlide: 0,
+        },
+      },
+    ],
+  };
+
+  return (
+    <div className="">
+      {/* TOP TITLE */}
+      <div className="title-16-semibold text-secondary-2 mb-6 flex items-center gap-2.5">
+        <div className="bg-secondary-2 h-10 w-5 rounded-sm"></div>
+        <p>Categories</p>
+      </div>
+
+      <div className="mb-10 flex items-center justify-between">
+        <div className="flex flex-col gap-2.5 md:flex-row md:items-center md:gap-20">
+          <h2 className="heading-36-semibold">Brows By Categories</h2>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <button
+            onClick={goToPrevSlide}
+            className="bg-secondary flex h-[46px] w-[46px] cursor-pointer items-center justify-center rounded-full"
+          >
+            <FaArrowLeft className="text-text-2 text-2xl" />
+          </button>
+
+          <button
+            onClick={goToNextSlide}
+            className="bg-secondary flex h-[46px] w-[46px] cursor-pointer items-center justify-center rounded-full"
+          >
+            <FaArrowRight className="text-text-2 text-2xl" />
+          </button>
+        </div>
+      </div>
+
+      <CategorySlider settings={settings} sliderRef={sliderRef} />
+    </div>
+  );
+};
+
+export default BrowsByCategories;
---
+++ src/features/products/components/browseByCategories/CategorySlider.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import Slider from "react-slick";
+import CategoryCard from "./CategoryCard";
+
+import { GiSmartphone } from "react-icons/gi";
+
+const categories = [
+  {
+    id: 1,
+    title: "Smartphone",
+    icon: GiSmartphone,
+  },
+  {
+    id: 2,
+    title: "Smartphone",
+    icon: GiSmartphone,
+  },
+  {
+    id: 3,
+    title: "Smartphone",
+    icon: GiSmartphone,
+  },
+  {
+    id: 4,
+    title: "Smartphone",
+    icon: GiSmartphone,
+  },
+  {
+    id: 5,
+    title: "Smartphone",
+    icon: GiSmartphone,
+  },
+  {
+    id: 6,
+    title: "Smartphone",
+    icon: GiSmartphone,
+  },
+];
+
+function CategorySlider({ settings, sliderRef }) {
+  return (
+    <div className="-mx-[15px]">
+      <Slider ref={sliderRef} {...settings}>
+        {categories.map((category) => (
+          <div className="px-[15px]">
+            <div className="">
+              <CategoryCard icon={category.icon} title={category.title} />
+            </div>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+}
+
+export default CategorySlider;
