1fa0ac79bf0f53ce4249f43665dccdd636f0192d
Shakibul Islam <shakib97itis@gmail.com>
2025-08-10 21:19:01 +0600

    feat: add route for ProductsPage to enhance product navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router";
 import Home from "./pages/home/Home";
+import ProductsPage from "./pages/productsPage/ProductsPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/products" element={<ProductsPage />} />
     </Routes>
   );
 }
