0f215d2773cf3a3e863209bb14a82af0b98888c1
Shakib <41319856+shakib97itis@users.noreply.github.com>
2025-08-19 23:46:32 +0600

    feat: update routing to replace ProductsPage with ShopPage and streamline navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { Route, Routes } from "react-router";
 import Home from "./pages/home/Home";
-import ProductsPage from "./pages/productsPage/ProductsPage";
+import ShopPage from "./pages/shopPage/ShopPage";
+// import ProductDetails from "./pages/ProductDetails/Product";
+import WishListPage from "./pages/wishListPage/WishListPage";
+import CartPage from "./pages/cart/CartPage";
+import { Check } from "lucide-react";
+import CheckOutPage from "./pages/checkOutPage/CheckOutPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/products" element={<ProductsPage />} />
+      <Route path="/shop" element={<ShopPage />} />
+      {/* <Route path="/shop/:id" element={<ProductDetails />} /> */}
+      <Route path="/cart" element={<CartPage />} />
+      <Route path="/checkout" element={<CheckOutPage />} />
+      <Route path="/wishlist" element={<WishListPage />} />
+      <Route path="*" element={<h1>404</h1>} />
     </Routes>
   );
 }
